d8d085249aef8a83142d1940aaadc11b9167bee1
DaiF <julie.fiadino@gmail.com>
2024-07-16 21:26:08 +0200

    update(main-app): add helper text and update page head

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,28 @@ function App() {
     const [boneCount, setBoneCount] = useState<number>(1);
 
     return (
-        <>
-        <Creature boneCount={boneCount}></Creature>
+        <div id="main-app">
+            <header>
+                <h2 id="helper-text">Drag the red dot to move the creature</h2>
+            </header>
 
-        <div id="config-panel">
-            <p>Bone Count</p>
-            <div className="config-option">
-                <button onClick={() => setBoneCount(boneCount + 1)}>
-                    <FontAwesomeIcon icon={faPlus} />
-                </button>
-                <p>{boneCount}</p>
-                <button onClick={() => { if (boneCount > 1) setBoneCount(boneCount - 1)}}>
-                    <FontAwesomeIcon icon={faMinus} />
-                </button>
+            <Creature boneCount={boneCount}></Creature>
+
+            <div id="config-panel">
+                <h3>Control Panel</h3>
+
+                <p>Bone Count</p>
+                <div className="config-option">
+                    <button onClick={() => setBoneCount(boneCount + 1)}>
+                        <FontAwesomeIcon icon={faPlus} />
+                    </button>
+                    <p>{boneCount}</p>
+                    <button onClick={() => { if (boneCount > 1) setBoneCount(boneCount - 1)}}>
+                        <FontAwesomeIcon icon={faMinus} />
+                    </button>
+                </div>
             </div>
         </div>
-        </>
     )
 }
 
